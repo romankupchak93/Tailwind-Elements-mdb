dd0141751c0dd889dfff47ed8daf0af1053ae479
Mikolaj Smolenski <smolenski.mikolaj@gmail.com>
2023-02-01 17:40:27 +0100

    Dark mode ssr

--- site/static/js/theme.js
+++ site/static/js/theme.js
@@ -1,48 +1,53 @@
 class ThemeSwitcher {
   constructor(element) {
     this.element = element;
-    this.themeSwitcherButton = this.element.querySelector('button');
-    this.themeSwitcherItems = this.element.querySelectorAll('a');
+    this.themeSwitcherButton = this.element.querySelector("button");
+    this.themeSwitcherItems = this.element.querySelectorAll("a");
 
     this.init();
   }
 
   init() {
-    if (!('theme' in localStorage)) {
-      this.setLightTheme();
-      // this.setSystemTheme();
-    } else if (localStorage.theme === 'dark') {
-      this.setDarkTheme();
-    } else {
-      this.setLightTheme();
-    }
-
+    console.log(this.getCookie("theme"));
     this.addEventListeners();
   }
 
   setSystemTheme() {
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      document.documentElement.classList.add('dark');
-      this.setActiveThemeIcon('dark');
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      document.documentElement.classList.add("dark");
+      this.setActiveThemeIcon("dark");
+      document.cookie = `theme=dark; expires=${new Date(
+        new Date().getTime() + 1000 * 60 * 60 * 24 * 365
+      ).toGMTString()}; path=/`;
     } else {
-      document.documentElement.classList.remove('dark');
-      this.setActiveThemeIcon('light');
+      document.documentElement.classList.remove("dark");
+      this.setActiveThemeIcon("light");
+      document.cookie = `theme=light; expires=${new Date(
+        new Date().getTime() + 1000 * 60 * 60 * 24 * 365
+      ).toGMTString()}; path=/`;
     }
-    this.setActiveDropdownItem('system');
+    this.setActiveDropdownItem("system");
+    document.cookie = `systemTheme=true; expires=${new Date(
+      new Date().getTime() + 1000 * 60 * 60 * 24 * 365
+    ).toGMTString()}; path=/`;
   }
 
   setDarkTheme() {
-    document.documentElement.classList.add('dark');
-    localStorage.theme = 'dark';
-    this.setActiveThemeIcon('dark');
-    this.setActiveDropdownItem('dark');
+    document.documentElement.classList.add("dark");
+    this.setActiveThemeIcon("dark");
+    this.setActiveDropdownItem("dark");
+    document.cookie = `theme=dark; expires=${new Date(
+      new Date().getTime() + 1000 * 60 * 60 * 24 * 365
+    ).toGMTString()}; path=/`;
   }
 
   setLightTheme() {
-    document.documentElement.classList.remove('dark');
-    localStorage.theme = 'light';
-    this.setActiveThemeIcon('light');
-    this.setActiveDropdownItem('light');
+    document.documentElement.classList.remove("dark");
+    this.setActiveThemeIcon("light");
+    this.setActiveDropdownItem("light");
+    document.cookie = `theme=light; expires=${new Date(
+      new Date().getTime() + 1000 * 60 * 60 * 24 * 365
+    ).toGMTString()}; path=/`;
   }
 
   setActiveThemeIcon(theme) {
@@ -51,24 +56,46 @@ class ThemeSwitcher {
     ).innerHTML;
   }
 
+  getCookie(cookieName) {
+    let name = cookieName + "=";
+    let cookieArray = document.cookie.split(";");
+
+    for (let i = 0; i < cookieArray.length; i++) {
+      let c = cookieArray[i];
+      while (c.charAt(0) == " ") {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
   setActiveDropdownItem(theme) {
-    this.element.querySelectorAll('[data-theme-icon]').forEach((item) => {
-      item.classList.remove('text-blue-500');
+    this.element.querySelectorAll("[data-theme-icon]").forEach((item) => {
+      item.classList.remove("text-blue-500");
     });
-    this.element.querySelectorAll('[data-theme-name]').forEach((item) => {
-      item.classList.remove('text-blue-500');
+    this.element.querySelectorAll("[data-theme-name]").forEach((item) => {
+      item.classList.remove("text-blue-500");
     });
-    this.element.querySelector(`[data-theme-icon=${theme}]`).classList.add('text-blue-500');
-    this.element.querySelector(`[data-theme-name=${theme}]`).classList.add('text-blue-500');
+    this.element
+      .querySelector(`[data-theme-icon=${theme}]`)
+      .classList.add("text-blue-500");
+    this.element
+      .querySelector(`[data-theme-name=${theme}]`)
+      .classList.add("text-blue-500");
   }
 
   onThemeSwitcherItemClick(event) {
     const theme = event.target.dataset.theme;
 
-    if (theme === 'system') {
-      localStorage.removeItem('theme');
+    if (theme === "system") {
+      document.cookie = `theme=; expires=${new Date(
+        new Date().getTime() - 1000
+      ).toGMTString()}; path=/`;
       this.setSystemTheme();
-    } else if (theme === 'dark') {
+    } else if (theme === "dark") {
       this.setDarkTheme();
     } else {
       this.setLightTheme();
@@ -76,15 +103,16 @@ class ThemeSwitcher {
   }
 
   addEventListeners() {
-    const bindedOnThemeSwitcherItemClick = this.onThemeSwitcherItemClick.bind(this);
+    const bindedOnThemeSwitcherItemClick =
+      this.onThemeSwitcherItemClick.bind(this);
 
     this.themeSwitcherItems.forEach((item) => {
-      item.addEventListener('click', bindedOnThemeSwitcherItemClick);
+      item.addEventListener("click", bindedOnThemeSwitcherItemClick);
     });
   }
 }
 
-const themeSwitcher = document.querySelector('#theme-switcher');
+const themeSwitcher = document.querySelector("#theme-switcher");
 
 if (themeSwitcher) {
   new ThemeSwitcher(themeSwitcher); // eslint-disable-line no-new
--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   content: [
     "./layouts/**/*.html",
+    "./themes/**/*.html",
     "./content/**/*.html",
     "./static/**/*.js",
     "../src/**/*.js",
